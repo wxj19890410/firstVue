9ad4a3759085a20a05442e60e6f47ff147f08861
wxj19890410 <1213510363@qq.com>
2018-10-28 00:12:17 +0800

    a

---
+++ src/pages/Manage.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="manage_page fillcontain">
+		<el-row style="height: 100%;">
+	  		<el-col :span="4"  style="min-height: 100%; background-color: #324057;">
+               <el-menu :default-active="defaultActive" style="min-height: 100%"theme="dark" router>
+					<el-menu-item index="manage"><i class="el-icon-menu"></i>首页</el-menu-item>
+					<el-submenu index="2">
+						<template slot="title"><i class="el-icon-document"></i>数据管理</template>
+						<el-menu-item index="addData">添加数据</el-menu-item>
+					</el-submenu>
+					<el-submenu index="3">
+						<template slot="title"><i class="el-icon-plus"></i>历史纪录</template>
+						<el-menu-item index="login">历史Excel</el-menu-item>
+						<el-menu-item index="login">添加商品</el-menu-item>
+					</el-submenu>
+					<el-submenu index="6">
+						<template slot="title"><i class="el-icon-setting"></i>设置</template>
+						<el-menu-item index="login">管理员设置</el-menu-item>
+						<el-menu-item index="login">个人信息</el-menu-item>
+						<!-- <el-menu-item index="sendMessage">发送通知</el-menu-item> -->
+					</el-submenu>
+				</el-menu>
+			</el-col>
+			<el-col :span="20" style="height: 100%;overflow: auto;">
+				<keep-alive>
+				    <router-view></router-view>
+				</keep-alive>
+			</el-col>
+		</el-row>
+  	</div>
+</template>
+
+<script>
+    export default {
+		computed: {
+			defaultActive: function(){
+				return this.$route.path.replace('/', '');
+			}
+		},
+    }
+</script>
+
+
+<style lang="less" scoped>
+	@import '../style/mixin';
+	.manage_page{
+		
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/Login'
+import Manage from '@/pages/Manage'
+import AddData from '@/pages/AddData'
 
 Vue.use(Router)
 
@@ -10,6 +12,19 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    }, {
+      path: '/manage',
+      name: 'Manage',
+      component: Manage,
+      children: [{
+        path: '',
+        component: AddData,
+        meta: [],
+      }, {
+        path: '/addData',
+        component: AddData
+      }]
     }
+
   ]
 })
